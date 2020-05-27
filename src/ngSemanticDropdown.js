b434fe3a106e5ac8091f76eaf9612b62e4cb3b10
Brock Barlow <bbarlow@ifsa-butler.org>
2020-05-27 10:02:24 -0400

    Added Files

---
+++ src/ngSemanticDropdown.js
@@ -0,0 +1,79 @@
+angular.module('ngSemanticDirectives', [])
+.directive('ngDropdown', [
+    function () {
+        return {
+            restrict: 'E',
+            scope: {
+                selected: '=',
+                options: '=',
+                multiple: '=',
+                change: '&',
+                disabled: '=',
+                additions: '=',
+                loading: '=',
+                defaultText: '@',
+                labelField: '@'
+
+            },
+            //templateUrl: 'ngDropDown.html',
+            template:`
+                <div class="ui dropdown selection" ng-class="{'loading' : loading, 'disabled' : disabled, 'multiple' : multiple, 'active visible' : isActive }" tabindex="0" ng-blur="onBlur()">
+                    <i ng-click="isActive = !isActive" class="dropdown icon"></i>
+                    <div class="default text" ng-if="(!multiple && !selected) || (multiple && !selected.length)" ng-click="isActive = !isActive" ng-bind="defaultText"></div>
+                    <div class="text" ng-if="selected && !multiple" ng-bind="selected[labelField]"></div>
+                    <a ng-if="selected && selected.length" style="display: inline-block !important;" class="ui label transition visible" ng-repeat="item in selected">
+                        {{item[labelField]}}
+                        <i class="delete icon" ng-click="selected.splice($index, 1)"></i>
+                    </a>
+                    <div class="menu transition" ng-class="isActive ? 'visible menu-visible' : 'hidden'" >
+                        <div class="item" ng-click="selectOption(item)" ng-repeat="item in options | filter:isUnselected" ng-bind="item[labelField]"></div>
+                    </div>
+                </div>
+                <style>
+                    .menu-visible{
+                        display: block !important;
+                    }
+                </style>`,
+            link: function (scope, elem, attrs) {
+                if(scope.multiple && !Array.isArray(scope.selected)){
+                    scope.selected = [];
+                }
+                if(scope.change){
+                    scope.$watch('selected', function(newVal, oldVal){
+                        if((!scope.multiple && scope.selected) || (scope.multiple && !scope.selected.length)){
+                            scope.change();
+                        }
+                        
+                    })
+                }
+                scope.changeActive = function(){
+                    scope.isActive = !scope.isActive;
+                }
+
+                scope.getLabel = function(item){
+                    return item[scope.labelField];
+                }
+                scope.selectOption = function(item){
+                    if(!scope.multiple){
+                        scope.selected = item;
+                        scope.isActive = false;
+                    }
+                    else{
+                        scope.selected.push(item);
+                    }
+                    
+                }
+                scope.onBlur = function(){
+                    scope.isActive = false;
+                }
+                scope.isUnselected = function(item){
+                    if(scope.multiple){
+                        scope.isActive = scope.isActive;
+                        return scope.selected.findIndex(i => i == item) === -1;
+                    }
+                    return true;
+                }
+            }
+        };
+    }]
+);
